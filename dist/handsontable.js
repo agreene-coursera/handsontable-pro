7b54f43a7e2469f6aeb17416d1e22581e53d3b13
Austin Greene <agreene@coursera.org>
2018-05-17 13:13:13 -0700

    BUILD

--- dist/handsontable.js
+++ dist/handsontable.js
@@ -21,7 +21,7 @@
  * UNINTERRUPTED OR ERROR FREE.
  * 
  * Version: 2.0.0
- * Release date: 11/04/2018 (built at 17/05/2018 12:53:43)
+ * Release date: 11/04/2018 (built at 17/05/2018 13:11:02)
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -37546,7 +37546,7 @@ Handsontable.DefaultSettings = _defaultSettings2.default;
 Handsontable.EventManager = _eventManager2.default;
 Handsontable._getListenersCounter = _eventManager.getListenersCounter; // For MemoryLeak tests
 
-Handsontable.buildDate = '17/05/2018 12:53:43';
+Handsontable.buildDate = '17/05/2018 13:11:02';
 Handsontable.packageName = 'handsontable-pro';
 Handsontable.version = '2.0.0';
 
